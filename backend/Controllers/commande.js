763edad648885daa1622e14a66ebb1c5f92f31cc
“issam <hamdiissam1997@gmail.com>
2023-02-22 16:29:34 +0100

    comparison date_achat et  date_livraison

--- backend/Controllers/commande.js
+++ backend/Controllers/commande.js
@@ -9,9 +9,40 @@ const {
 const { StatusCodes } = require("http-status-codes");
 
 // create Commande
+
 const createCommandeController = async (req, res) => {
-  const da = await createCommandeService({ ...req.body });
-  res.status(StatusCodes.CREATED).send(da);
+  const date_achat =req.body.date_achat;
+  const date_livraison =req .body.date_livraison
+  
+const a=new Date ()
+a.setUTCHours(0,0,0,0)
+
+const b=new Date(date_achat)
+b.getUTCHours(0,0,0,0)
+
+const c= new Date(date_livraison)
+c.getUTCHours(0,0,0,0)
+
+ if (a.getTime() <= b.getTime()){
+
+
+  if (c.getTime()>=b.getTime()) {
+    
+    const da= await createCommandeService({...req.body })
+     res.status(StatusCodes.CREATED).send(da);
+  } 
+  else{
+    res.send({msg:"date_livraison "})
+  }
+ 
+  }
+  
+  else{
+   res.send({msg:"date_achat "})
+
+ }
+
+ 
 };
 
 // getAll Commande
